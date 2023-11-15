d50a3a1bf26dbbe6ffdd85634d098e4fd672f746
alexandrobol <qweufibeze@gmail.com>
2023-11-15 19:50:56 +0300

    qwe

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
 
-import 'switch_widget.dart';
+void main() => runApp(const MyApp());
 
-void main() => runApp(const SwitchApp());
-
-class SwitchApp extends StatelessWidget {
-  const SwitchApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const Scaffold(
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 425,
-              ),
-              SwitchWidget()
-            ],
-          ),
-        ),
+      title: 'My Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello, Flutter!');
   }
 }
